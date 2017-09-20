919c5cec5ecd855ff170a0083d77ed1a0e78c259
George <ultravail@users.noreply.github.com>
2017-09-20 23:13:34 +0200

    Wrap TW5 loading into jQuery ready function

--- bautagebuch.js
+++ bautagebuch.js
@@ -1,5 +1,7 @@
-$("#header2-1u").load("bautagebuch-blog.html", function(response, status, xhr) {
-  if (status == "error") {
-    $("#header2-1u").text("Unable to load: " + xhr.status + " " + xhr.statusText);
-  }
+$(function() {
+  $("#header2-1u").load("bautagebuch-blog.html", function(response, status, xhr) {
+    if (status == "error") {
+      $("#header2-1u").text("Unable to load: " + xhr.status + " " + xhr.statusText);
+    }
+  });
 });
