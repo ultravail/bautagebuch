1109d2a789491b5a22652e29d8bbcfcb95509ed8
George <ultravail@users.noreply.github.com>
2017-09-20 23:08:57 +0200

    Initial version of blog script for loading TW5 blog

---
+++ bautagebuch.js
@@ -0,0 +1,5 @@
+$("#header2-1u").load("bautagebuch-blog.html", function(response, status, xhr) {
+  if (status == "error") {
+    $("#header2-1u").text("Unable to load: " + xhr.status + " " + xhr.statusText);
+  }
+});
